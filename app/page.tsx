8df8b9bd440c746d40bf5913416d3fcfbdc160c8
Grant Diamond <grantadiamond@gmail.com>
2024-01-23 15:03:22 -0800

    console

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log("HELLO!")
   return (
     <main className={styles.main}>
       <div className={styles.description}>
